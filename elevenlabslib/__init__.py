84b20efc9b6a4da902863b81b7c34c0499c8b585
lugia19 <lugia19@lugia19.com>
2023-04-30 14:45:10 +0200

    Add the professional voice to the available classes.

--- elevenlabslib/__init__.py
+++ elevenlabslib/__init__.py
@@ -2,6 +2,7 @@ from .ElevenLabsUser import ElevenLabsUser
 from .ElevenLabsVoice import ElevenLabsVoice
 from .ElevenLabsVoice import ElevenLabsDesignedVoice
 from .ElevenLabsVoice import ElevenLabsClonedVoice
+from .ElevenLabsVoice import ElevenLabsProfessionalVoice
 from .ElevenLabsSample import ElevenLabsSample
 from .ElevenLabsHistoryItem import ElevenLabsHistoryItem
-__all__ = ["ElevenLabsUser", "ElevenLabsVoice","ElevenLabsClonedVoice", "ElevenLabsDesignedVoice", "ElevenLabsSample", "ElevenLabsHistoryItem"]
+__all__ = ["ElevenLabsUser", "ElevenLabsVoice","ElevenLabsClonedVoice", "ElevenLabsDesignedVoice","ElevenLabsProfessionalVoice", "ElevenLabsSample", "ElevenLabsHistoryItem"]
